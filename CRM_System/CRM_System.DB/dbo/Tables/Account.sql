8170b20a47328f5d74e47aa1395302ed518af049
lena falina <aternetas@gmail.com>
2022-07-28 18:18:29 +0300

    add tables 'Lead' & 'Account'

---
+++ CRM_System/CRM_System.DB/dbo/Tables/Account.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Account]
+(
+	[Id] INT IDENTITY (1, 1) NOT NULL PRIMARY KEY,
+	[Currency] NVARCHAR(10) NOT NULL,
+	[Status] NVARCHAR(10) NOT NULL,
+	[LeadId] INT NOT NULL,
+	[IsDeleted] BIT DEFAULT 0 NOT NULL,
+	FOREIGN KEY ([LeadId]) REFERENCES [dbo].[Lead] ([Id])
+);
---
+++ CRM_System/CRM_System.DB/dbo/Tables/Lead.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Lead]
+(
+	[Id] INT IDENTITY (1, 1) NOT NULL PRIMARY KEY,
+	[FirstName] NVARCHAR (50) NOT NULL,
+    [LastName]  NVARCHAR (50) NOT NULL,
+    [Patronymic]  NVARCHAR (50) NOT NULL,
+    [Birthday] DATE NOT NULL,
+    [Email]     NVARCHAR (50) NOT NULL,
+    [Phone]     NVARCHAR (25) NOT NULL,
+    [Passport] NVARCHAR(20) NOT NULL,
+    [Address] NVARCHAR(100) NOT NULL,
+    [Role] NVARCHAR(20) NOT NULL,
+    [IsDeleted] BIT DEFAULT 0 NOT NULL
+)
