309524f1f498d86a697313597e52350e9d34e170
lena falina <aternetas@gmail.com>
2022-07-29 18:04:13 +0300

    add LeadDTO

---
+++ CRM_System/CRM_DataLayer/Models/LeadDTO.cs
@@ -0,0 +1,18 @@
+﻿using CRM_System.BusinessLayer;
+
+namespace CRM.DataLayer.Models;
+
+public class LeadDTO
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Patronymic { get; set; }
+    public DateTime Birthday { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public int Passport { get; set; }
+    public string Address { get; set; }
+    public Role Role { get; set; }
+    public bool IsDeleted { get; set; }
+}
