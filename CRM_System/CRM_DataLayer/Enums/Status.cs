62f76f1cf8114e755e67cf011f76c67f27e1db40
Daria Shadrina <darinashadrina@yandex.ru>
2022-08-01 16:06:21 +0300

    Add request model for Account

--- CRM_System/CRM_DataLayer/Enums/Status.cs
+++ CRM_System/CRM_DataLayer/Enums/Status.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CRM.DataLayer.Enum
+namespace CRM.DataLayer.Enums
 {
     public enum Status
     {
--- CRM_System/CRM_System.BusinessLayer/Models/UserModel.cs
+++ CRM_System/CRM_System.BusinessLayer/Models/UserModel.cs
@@ -5,9 +5,6 @@ namespace CRM_System.BusinessLayer;
 public class UserModel
 {
     public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Patronymic { get; set; }
     public Role Role { get; set; }
     public string Password { get; set; }
     public bool isDeleted { get; set; }
