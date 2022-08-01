7efe7c24af18e1aeed87e0e327d6f282d6053cd8
lena falina <aternetas@gmail.com>
2022-08-01 15:53:03 +0300

    add ClaimModel

---
+++ CRM_System/CRM_System.BusinessLayer/Models/ClaimModel.cs
@@ -0,0 +1,11 @@
+﻿using CRM.DataLayer;
+
+namespace CRM_System.BusinessLayer;
+
+public class ClaimModel
+{
+    public int Id { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public Role Role { get; set; }
+}
