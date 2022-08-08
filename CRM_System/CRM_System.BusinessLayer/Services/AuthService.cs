af4a7e34488fe67d3db54440d61b646db4e1ff84
Daria Shadrina <darinashadrina@yandex.ru>
2022-08-08 12:57:47 +0300

    Add method login

--- CRM_System/CRM_System.BusinessLayer/Services/AuthService.cs
+++ CRM_System/CRM_System.BusinessLayer/Services/AuthService.cs
@@ -1,6 +1,4 @@
 ﻿using CRM.DataLayer.Interfaces;
-using CRM_System.Business_Layer.Infrastucture;
-using CRM_System.BusinessLayer.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
@@ -10,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.IdentityModel.Tokens;
 using CRM_System.BusinessLayer.Models;
+using CRM_System.BusinessLayer.Infrastuctures;
+using System.Data;
+using CRM.DataLayer;
 
 namespace CRM_System.BusinessLayer.Services
 {
@@ -49,7 +50,27 @@ namespace CRM_System.BusinessLayer.Services
 
         public ClaimModel Login (LoginRequest loginRequest)
         {
+            ClaimModel claimModel = new ClaimModel ();
 
+            var lead = _leadsRepository.GetByEmail(loginRequest.Login);
+
+            if (lead is not null && loginRequest.Login == lead.Result.Email && 
+                PasswordHash.ValidatePassword(loginRequest.Password, lead.Result.Password) && !lead.Result.IsDeleted)
+
+            {
+                claimModel.Email = loginRequest.Login;
+                if (lead.Result.Role == Role.Regular)
+
+                {
+
+                    claimModel.Role = Role.Regular;
+
+                }
+
+                else claimModel.Role = Role.Vip;
+
+            }
+            return claimModel;
         }
     }
 }
