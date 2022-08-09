7d8d7a16afc272353679b53baaf915e81380f1c0
lena falina <aternetas@gmail.com>
2022-08-09 20:42:09 +0300

    add claims in controller & service

--- CRM_System/CRM_System.API/Controllers/LeadsController.cs
+++ CRM_System/CRM_System.API/Controllers/LeadsController.cs
@@ -15,6 +15,7 @@ public class LeadsController : ControllerBase
 {
     private readonly ILeadsService _leadsService;
     private readonly IMapper _mapper;
+    private ClaimModel _claims;
 
     public LeadsController(ILeadsService leadsService, IMapper mapper)
     {
@@ -43,15 +44,16 @@ public class LeadsController : ControllerBase
     [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
     public async Task<ActionResult<LeadMainInfoResponse>> GetById(int id)
     {
-        var result = await _leadsService.GetById(id);
+        var claims = this.GetClaims();
+        var lead = await _leadsService.GetById(id, claims);
 
-        if (result is null)
+        if (lead is null)
             return NotFound();
         else
-            return Ok(_mapper.Map<LeadMainInfoResponse>(result));
+            return Ok(_mapper.Map<LeadMainInfoResponse>(lead));
     }
 
-    [AuthorizeByRole(Role.Regular, Role.Vip)]
+    [AuthorizeByRole]
     [HttpGet]
     [ProducesResponseType(typeof(List<LeadMainInfoResponse>), StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(void), StatusCodes.Status403Forbidden)]
@@ -59,7 +61,11 @@ public class LeadsController : ControllerBase
     public async Task<ActionResult<List<LeadMainInfoResponse>>> GetAll()
     {
         var leads = await _leadsService.GetAll();
-        return Ok(_mapper.Map<List<LeadAllInfoResponse>>(leads));
+
+        if (leads is null)
+            return NotFound();
+        else
+            return Ok(_mapper.Map<List<LeadAllInfoResponse>>(leads));
     }
 
     [AuthorizeByRole(Role.Regular, Role.Vip)]
@@ -69,9 +75,22 @@ public class LeadsController : ControllerBase
     [ProducesResponseType(typeof(void), StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(typeof(void), StatusCodes.Status403Forbidden)]
     [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
-    public async Task<ActionResult> Update([FromBody] LeadUpdateRequest request)
+    public async Task<ActionResult> Update([FromBody] LeadUpdateRequest request, int id)
     {
-        await _leadsService.Update(_mapper.Map<LeadDto>(request));
+        var claims = this.GetClaims();
+
+        var newLead = new LeadDto()
+        {
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            Patronymic = request.Patronymic,
+            Birthday = request.Birthday,
+            Phone = request.Phone,
+            City = request.City,
+            Address = request.Address
+        };
+
+        await _leadsService.Update(newLead, id, claims);
         return NoContent();
     }
 
@@ -84,8 +103,14 @@ public class LeadsController : ControllerBase
     [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
     public async Task<ActionResult> Remove(int id)
     {
-        await _leadsService.DeleteOrRestore(id, true);
-        return NoContent();
+        var claims = this.GetClaims();
+        var lead = await _leadsService.GetById(id, claims);
+
+        if (lead is null)
+            return NotFound();
+        else
+            await _leadsService.DeleteOrRestore(id, true, claims);
+            return NoContent();
     }
 
     [AuthorizeByRole(Role.Regular, Role.Vip)]
@@ -97,7 +122,13 @@ public class LeadsController : ControllerBase
     [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
     public async Task<ActionResult> Restore(int id)
     {
-        await _leadsService.DeleteOrRestore(id, false);
-        return NoContent();
+        var claims = this.GetClaims();
+        var lead = await _leadsService.GetById(id, claims);
+
+        if (lead is null)
+            return NotFound();
+        else
+            await _leadsService.DeleteOrRestore(id, false, claims);
+            return NoContent();
     }
 }
--- CRM_System/CRM_System.BusinessLayer/Services/Interfaces/ILeadsService.cs
+++ CRM_System/CRM_System.BusinessLayer/Services/Interfaces/ILeadsService.cs
@@ -5,9 +5,9 @@ namespace CRM_System.BusinessLayer;
 public interface ILeadsService
 {
     Task<int> Add(LeadDto lead);
-    Task<LeadDto> GetById(int id);
+    Task<LeadDto> GetById(int id, ClaimModel claims);
     Task<LeadDto?> GetByEmail(string email);
     Task<List<LeadDto>> GetAll();
-    Task Update(LeadDto lead);
-    Task DeleteOrRestore(int id, bool isDeleting);
+    Task Update(LeadDto lead, int id, ClaimModel claims);
+    Task DeleteOrRestore(int id, bool isDeleting, ClaimModel claims);
 }
