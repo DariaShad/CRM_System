2291085963fc03c3732dce5b148003301da6a552
Daria Shadrina <darinashadrina@yandex.ru>
2022-08-10 17:50:09 +0300

    Add claims in controllers and method for checking access

--- CRM_System/CRM_System.BusinessLayer/Services/AccountsService.cs
+++ CRM_System/CRM_System.BusinessLayer/Services/AccountsService.cs
@@ -1,4 +1,5 @@
-﻿using CRM.DataLayer.Interfaces;
+﻿using CRM.DataLayer;
+using CRM.DataLayer.Interfaces;
 using CRM.DataLayer.Models;
 using CRM_System.BusinessLayer.Services.Interfaces;
 
@@ -12,34 +13,49 @@ namespace CRM_System.BusinessLayer.Services
         {
             _accountRepository = accountRepository;
         }
-        public int AddAccount(AccountDto accountDTO)
+        public int AddAccount(AccountDto accountDTO, ClaimModel claim)
         {
+            CheckAccessForLeadAndManager(accountDTO.Id, claim);
             var result = _accountRepository.AddAccount(accountDTO);
             return result;
         }
 
-        public void DeleteAccount(int id)
+        public void DeleteAccount(int id, ClaimModel claim)
         {
             _accountRepository.DeleteAccount(id);
+            CheckAccessForLeadAndManager(id, claim);
         }
 
         public AccountDto GetAccountById(int id, ClaimModel claim)
         {
+            CheckAccessForLeadAndManager(id, claim); //Написано неправильно!!! нужно переделать под айди ЛИДА
             var result = _accountRepository.GetAccountById(id);
             return result;
         }
 
         public List<AccountDto> GetAllAccounts() => _accountRepository.GetAllAccounts().ToList();
 
-        public List<AccountDto> GetAllAccountsByLeadId(int leadId)
+        public List<AccountDto> GetAllAccountsByLeadId(int leadId, ClaimModel claim)
         {
+            CheckAccessForLeadAndManager(leadId, claim);
             var result = _accountRepository.GetAllAccounts();
             return result;
         }
 
-        public void UpdateAccount(AccountDto account, int id)
+        public void UpdateAccount(AccountDto account, int id, ClaimModel claim)
         {
+            CheckAccessForLeadAndManager(id, claim);
             _accountRepository.UpdateAccount(account, id);
+        }
+
+        private async Task CheckAccessForLeadAndManager(int id, ClaimModel claims)
+        {
+            if (claims is not null && claims.Id != id ||
+                claims.Role != Role.Admin)
+            {
+                throw new AccessDeniedException($"Access denied");
+            }    
+                
         }
 
     }
--- CRM_System/CRM_System.BusinessLayer/Services/Interfaces/IAccountsService.cs
+++ CRM_System/CRM_System.BusinessLayer/Services/Interfaces/IAccountsService.cs
@@ -4,11 +4,11 @@ namespace CRM_System.BusinessLayer.Services.Interfaces
 {
     public interface IAccountsService
     {
-        public int AddAccount(AccountDto accountDTO);
+        public int AddAccount(AccountDto accountDTO, ClaimModel claim);
         public List<AccountDto> GetAllAccounts();
         public AccountDto GetAccountById(int id, ClaimModel claim);
-        public List<AccountDto> GetAllAccountsByLeadId(int leadId);
-        public void UpdateAccount(AccountDto account, int id);
-        public void DeleteAccount(int id);
+        public List<AccountDto> GetAllAccountsByLeadId(int leadId, ClaimModel claim);
+        public void UpdateAccount(AccountDto account, int id, ClaimModel claim);
+        public void DeleteAccount(int id, ClaimModel claim);
     }
 }
