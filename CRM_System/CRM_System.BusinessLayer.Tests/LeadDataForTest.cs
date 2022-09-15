35ba140d8f2480505808f5113abbc8883d8b9bb9
Daria Shadrina <darinashadrina@yandex.ru>
2022-09-15 14:48:05 +0300

    Start test

---
+++ CRM_System/CRM_System.BusinessLayer.Tests/LeadDataForTest.cs
@@ -0,0 +1,60 @@
+﻿using CRM_System.DataLayer;
+using IncredibleBackendContracts.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM_System.BusinessLayer.Tests
+{
+    public static class LeadDataForTest
+    {
+        public static LeadDto GetRegularLeadDataForTest()
+        {
+            var lead = new LeadDto()
+            {
+                FirstName = "Lala",
+                LastName = "Lala",
+                Patronymic = "Lala",
+                Email = "lalalalf@mail.ru",
+                Address = "Lenina street, 34-3",
+                City = City.Moscow,
+                Passport = "2020 400400",
+                Password = "1234569",
+                Phone = "89996321578",
+                Birthday = DateTime.Parse("2000 - 12 - 12"),
+                RegistrationDate = DateTime.Now,
+                Role = Role.Regular,
+                IsDeleted = false,
+                Accounts = new List<AccountDto>()
+            };
+            return lead;
+
+        }
+
+        public static LeadDto GetVipLeadDataForTest()
+        {
+            var lead = new LeadDto()
+            {
+                FirstName = "Lala",
+                LastName = "Lala",
+                Patronymic = "Lala",
+                Email = "lalalalf@mail.ru",
+                Address = "Lenina street, 34-3",
+                City = City.Moscow,
+                Passport = "2020 400400",
+                Password = "1234569",
+                Phone = "89996321578",
+                Birthday = DateTime.Parse("2000 - 12 - 12"),
+                RegistrationDate = DateTime.Now,
+                Role = Role.Vip,
+                IsDeleted = false,
+                Accounts = new List<AccountDto>()
+            };
+            return lead;
+
+
+        }
+    }
+}
