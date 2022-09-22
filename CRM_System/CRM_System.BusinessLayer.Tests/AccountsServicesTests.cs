3ac20253bfb92c6ec61314ca2ac4b67941b340cc
Daria Shadrina <darinashadrina@yandex.ru>
2022-09-22 21:33:15 +0300

    Add test for adding account

--- CRM_System/CRM_System.BusinessLayer.Tests/AccountsServicesTests.cs
+++ CRM_System/CRM_System.BusinessLayer.Tests/AccountsServicesTests.cs
@@ -1,7 +1,55 @@
-﻿namespace CRM_System.BusinessLayer.Tests
+﻿using CRM_System.BusinessLayer.Tests.TestModels;
+using CRM_System.DataLayer;
+using IncredibleBackend.Messaging.Interfaces;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace CRM_System.BusinessLayer.Tests
 {
     public class AccountsServicesTests
     {
-       
+        private AccountsService _sut;
+        private Mock<ILeadsRepository> _leadsRepositoryMock;
+        private Mock<IAccountsRepository> _accountsRepositoryMock;
+        private ClaimModel _claimModel;
+        private Mock<ILogger<AccountsService>> _logger;
+        private Mock<IMessageProducer> _producer;
+
+        [SetUp]
+        public void Setup()
+        {
+            _accountsRepositoryMock = new Mock<IAccountsRepository>();
+            _producer = new Mock<IMessageProducer>();
+            _logger = new Mock<ILogger<AccountsService>>();
+            _leadsRepositoryMock = new Mock<ILeadsRepository>();
+            _sut = new AccountsService(_accountsRepositoryMock.Object, _logger.Object, _producer.Object, _leadsRepositoryMock.Object);
+        }
+
+        [Test]
+        public async Task AddAccount_WhenDataIsCorrect_ReturnId()
+        {
+            //given
+            var lead = LeadDataForTest.GetRegularLeadDataForTest();
+            _leadsRepositoryMock.Setup(l => l.Add(It.Is<LeadDto>(p => p.Id == lead.Id)))
+            .ReturnsAsync(lead.Id);
+            var account = AccountDataForTest.GetAccountDataForTest();
+            account.LeadId = lead.Id;
+            _accountsRepositoryMock.Setup(a => a.AddAccount(It.Is<AccountDto>(p => p.Id == account.Id)))
+            .ReturnsAsync(account.Id);
+
+            _claimModel = new ClaimModel()
+            {
+                Id = lead.Id,
+                Role = lead.Role
+            };
+
+            var expectedId = account.Id;
+
+            //when
+            var actual = await _sut.AddAccount(account, _claimModel);
+
+            //then
+            Assert.That(expectedId, Is.EqualTo(actual));
+        }
     }
 }
---
+++ CRM_System/CRM_System.BusinessLayer.Tests/TestModels/AccountDataForTest.cs
@@ -0,0 +1,24 @@
+﻿using CRM_System.DataLayer;
+using IncredibleBackendContracts.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM_System.BusinessLayer.Tests.TestModels
+{
+    public static class AccountDataForTest
+    {
+        public static AccountDto GetAccountDataForTest()
+        {
+            var account = new AccountDto()
+            {
+                Currency = TradingCurrency.RUB,
+                Status = AccountStatus.Active,
+                IsDeleted = false
+            };
+            return account;
+        }
+    }
+}
